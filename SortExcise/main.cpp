17a1eb12f1028bd8027f1cc58c0575e592cb8207
xsun <sunxin89@gmail.com>
2017-01-04 14:46:17 -0800

    checkin

--- SortExcise/main.cpp
+++ SortExcise/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void sortArray(int* arr, int length){
+void maopaosortArray(int* arr, int length){
     int temp;
     for (int i = 0; i < length-1; ++i) {
         for (int j = i+1; j < length; ++j) {
@@ -14,17 +14,31 @@ void sortArray(int* arr, int length){
             }
         }
     }
-    /*
-    for (int i = 0; i < length; ++i) {
-        cout << arr[i] << endl;
-    }
-    */
 }
+
+void gueibinMerge(int* arr, int low, int high)
+{
+
+}
+
+void gueibinSort(int* arr, int low, int high)
+{
+    int mid = (low + high)/2;
+    if(mid >= high){
+        return;
+    }
+
+    gueibinSort(arr, low, mid);
+    gueibinSort(arr, mid+1, high);
+    gueibinMerge(arr, low, high);
+
+}
+
 
 int main() {
     int arr[] = {10, 23, 56, 43, 25, 12, 5, 29, 41, 2};
     int len = sizeof(arr)/sizeof(*arr);
-    sortArray(arr, len);
+    maopaosortArray(arr, len);
     for (int i = 0; i < len; ++i) {
         cout << arr[i] << endl;
     }
